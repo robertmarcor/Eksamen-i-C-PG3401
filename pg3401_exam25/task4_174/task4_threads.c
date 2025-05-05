e139693e3fd53afd73d8a32562b33427c493708f
Robert M <r5m4r6@gmail.com>
2025-05-06 00:52:48 +0200

    made local variables

--- pg3401_exam25/task4_174/task4_threads.c
+++ pg3401_exam25/task4_174/task4_threads.c
@@ -8,14 +8,21 @@
 #define NUM_THREADS 2
 #define BYTE_RANGE 256
 
-int count[BYTE_RANGE];
-unsigned char buffer[BUFFER_SIZE];
-pthread_mutex_t mutex;
-pthread_cond_t cond_full, cond_empty;
-int bytes_in_buffer = 0;
+// wrapped global variables in a struct since pthread_create() only accepts void* arguments
+typedef struct
+{
+   int count[BYTE_RANGE];
+   unsigned char buffer[BUFFER_SIZE];
+   pthread_mutex_t mutex;
+   pthread_cond_t cond_full, cond_empty;
+   int bytes_in_buffer;
+} thread_data_t;
 
+// Now accepts thread_data_t struct pointer instead of using globals
+// All shared variables accessed through the data struct
 void *thread_A(void *arg)
 {
+   thread_data_t *data = (thread_data_t *)arg;
    FILE *fp = fopen("task4_pg2265.txt", "rb");
    if (!fp)
    {
@@ -25,62 +32,84 @@ void *thread_A(void *arg)
 
    while (1)
    {
-      pthread_mutex_lock(&mutex);
-      while (bytes_in_buffer == BUFFER_SIZE)
-         pthread_cond_wait(&cond_empty, &mutex);
+      pthread_mutex_lock(&(data->mutex));
+      while (data->bytes_in_buffer == BUFFER_SIZE)
+         pthread_cond_wait(&(data->cond_empty), &(data->mutex));
 
-      int read_bytes = fread(buffer + bytes_in_buffer, 1, BUFFER_SIZE - bytes_in_buffer, fp);
-      bytes_in_buffer += read_bytes;
+      int read_bytes = fread(data->buffer + data->bytes_in_buffer, 1,
+                             BUFFER_SIZE - data->bytes_in_buffer, fp);
+      data->bytes_in_buffer += read_bytes;
 
-      if (read_bytes < BUFFER_SIZE - bytes_in_buffer)
+      if (read_bytes < BUFFER_SIZE - data->bytes_in_buffer)
       {
-         pthread_mutex_unlock(&mutex);
+         pthread_mutex_unlock(&(data->mutex));
          break;
       }
-      pthread_cond_signal(&cond_full);
-      pthread_mutex_unlock(&mutex);
+      pthread_cond_signal(&(data->cond_full));
+      pthread_mutex_unlock(&(data->mutex));
    }
    fclose(fp);
    pthread_exit(NULL);
 }
 
+// Now accepts thread_data_t struct pointer instead of using globals
+// All shared variables accessed through the data struct
+// Count array is now part of the thread_data struct
 void *thread_B(void *arg)
 {
-   memset(count, 0, sizeof(count));
+   thread_data_t *data = (thread_data_t *)arg;
+   memset(data->count, 0, sizeof(data->count));
 
    while (1)
    {
-      pthread_mutex_lock(&mutex);
-      while (bytes_in_buffer == 0)
-         pthread_cond_wait(&cond_full, &mutex);
+      pthread_mutex_lock(&(data->mutex));
+      while (data->bytes_in_buffer == 0)
+         pthread_cond_wait(&(data->cond_full), &(data->mutex));
 
-      for (int i = 0; i < bytes_in_buffer; i++)
-         count[buffer[i]]++;
+      for (int i = 0; i < data->bytes_in_buffer; i++)
+         data->count[data->buffer[i]]++;
 
-      bytes_in_buffer = 0;
-      pthread_cond_signal(&cond_empty);
-      pthread_mutex_unlock(&mutex);
+      data->bytes_in_buffer = 0;
+      pthread_cond_signal(&(data->cond_empty));
+      pthread_mutex_unlock(&(data->mutex));
 
-      if (bytes_in_buffer == 0)
+      if (data->bytes_in_buffer == 0)
          break;
    }
    for (int i = 0; i < BYTE_RANGE; i++)
-      printf("%d: %d\n", i, count[i]);
+      printf("%d: %d\n", i, data->count[i]);
    pthread_exit(NULL);
 }
 
+// All previously global variables are now in main
+// Initializes the struct for shared data and allocates memory for it
+// Passes the thread_data struct to both threads
+// cleans up resources at the end
 int main(void)
 {
    pthread_t threadA, threadB;
-   void *memory_buffer = malloc(BUFFER_SIZE);
 
-   if (pthread_create(&threadA, NULL, thread_A, (void *)memory_buffer) != 0)
+   // Create and initialize the shared data
+   thread_data_t *thread_data = malloc(sizeof(thread_data_t));
+   if (!thread_data)
+   {
+      perror("Failed to allocate memory");
+      exit(EXIT_FAILURE);
+   }
+
+   // Initialize shared variables
+   thread_data->bytes_in_buffer = 0;
+   pthread_mutex_init(&(thread_data->mutex), NULL);
+   pthread_cond_init(&(thread_data->cond_full), NULL);
+   pthread_cond_init(&(thread_data->cond_empty), NULL);
+
+   if (pthread_create(&threadA, NULL, thread_A, (void *)thread_data) != 0)
    {
       perror("Could not create thread A");
       exit(1);
    }
 
-   if (pthread_create(&threadB, NULL, thread_B, (void *)memory_buffer) != 0)
+   if (pthread_create(&threadB, NULL, thread_B, (void *)thread_data) != 0)
    {
       perror("Could not create thread B");
       exit(1);
@@ -97,7 +126,11 @@ int main(void)
       exit(1);
    }
 
-   free(memory_buffer);
+   // Clean up
+   pthread_mutex_destroy(&(thread_data->mutex));
+   pthread_cond_destroy(&(thread_data->cond_full));
+   pthread_cond_destroy(&(thread_data->cond_empty));
+   free(thread_data);
 
    return 0;
 }
