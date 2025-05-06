775e17cdbd6cffbb3a4112f0287b036e2efbbd83
Batman <r5m4r6@gmail.com>
2025-05-05 20:26:46 -0400

    updated makefile and compiles

--- pg3401_exam25/task4_174/include/task4_threads.h
+++ pg3401_exam25/task4_174/include/task4_threads.h
@@ -1,6 +1,5 @@
-
-#define TASK4_THREADS_H
 #ifndef TASK4_THREADS_H
+#define TASK4_THREADS_H
 
 int Task2_SimpleDjb2Hash(FILE *fFileDescriptor, int *piHash);
 void encipher(unsigned int *const v, unsigned int *const w, const unsigned int *const k);
--- pg3401_exam25/task4_174/task4_threads.c
+++ pg3401_exam25/task4_174/task4_threads.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include "task4_threads.h"
 
 #define BUFFER_SIZE 4096
 #define NUM_THREADS 2
