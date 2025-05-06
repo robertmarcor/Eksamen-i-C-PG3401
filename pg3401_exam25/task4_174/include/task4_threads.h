e7555af52429cdcd0e21eef1211b66ad6a66e653
Batman <r5m4r6@gmail.com>
2025-05-05 20:22:49 -0400

    added header

---
+++ pg3401_exam25/task4_174/include/task4_threads.h
@@ -0,0 +1,8 @@
+
+#define TASK4_THREADS_H
+#ifndef TASK4_THREADS_H
+
+int Task2_SimpleDjb2Hash(FILE *fFileDescriptor, int *piHash);
+void encipher(unsigned int *const v, unsigned int *const w, const unsigned int *const k);
+
+#endif // TASK4_THREADS_H
