c742830da48417bd69156e2479b733df7bb45b02
Batman <r5m4r6@gmail.com>
2025-05-11 05:12:06 +0600

    Task6 Finished

--- pg3401_exam25/task6_174/include/main.h
+++ pg3401_exam25/task6_174/include/main.h
@@ -1,4 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void decipher(unsigned int *const v, unsigned int *const w, const unsigned int *const k);
+void encipher(unsigned int *const v, unsigned int *const w, const unsigned int *const k);
+
 #endif // MAIN_H
